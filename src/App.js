aa822ed001de8e866f28ef8db91cefb9ca605dd2
vamsi <vamsikrishnaperuri25@gmail.com>
2025-06-27 13:14:49 +0530

    v 0.1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import { useEffect } from 'react';
-// import './App.css';
-// import searchIcon from './search.svg';
-
-// const API_url = 'http://www.omdbapi.com/?i=tt3896198&apikey=b04a1857';
-
-// const App = () => {
-
-//   const searchMovies = async (title) => {
-//     const response = await fetch(`${API_url}&s=${title}`);
-//     const data = await response.json();
-//     console.log(data);
-//   }
-
-//   useEffect(() => {
-//     searchMovies('spiderman');
-//   }, []);
-
-//   return (
-//     <div className='app'>
-//       <h1>MovieLand</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useState, useEffect } from 'react';
 import './App.css';
 import searchIcon from './search.svg';
